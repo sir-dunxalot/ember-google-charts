6da6f55341260218b00185eb2cf7fe0daf249793
Duncan Graham Walker <walkerdu@bc.edu>
2015-10-04 14:00:32 -0400

    Test suite passing

--- addon/utils/render-material-chart.js
+++ addon/utils/render-material-chart.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { RSVP, run } = Ember;
+const { RSVP } = Ember;
 
 export default function renderMaterialChart({ charts, visualization }, data, options) {
   return new RSVP.Promise((resolve, reject) => {
@@ -8,8 +8,7 @@ export default function renderMaterialChart({ charts, visualization }, data, opt
     const chart = new charts[type](this.get('element'));
     const dataTable = visualization.arrayToDataTable(data);
 
-    visualization.events.addListener(chart, 'error', run.bind(this, reject));
-    visualization.events.addListener(chart, 'ready', run.bind(this, this.sendAction, 'chartDidRender'));
+    visualization.events.addListener(chart, 'error', reject);
 
     chart.draw(dataTable, charts[type].convertOptions(options));
 
