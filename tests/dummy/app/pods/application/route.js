1a2eb792ec561bc5e8e9f4323ac6bde3186c62bb
Duncan Graham Walker <walkerdu@bc.edu>
2015-10-04 17:54:53 -0400

    Nicer dummy app

--- tests/dummy/app/pods/application/route.js
+++ tests/dummy/app/pods/application/route.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   actions: {
+
+    linkTo(url) {
+      window.open(url);
+    },
+
     transitionTo(routeName) {
       const name = Ember.String.capitalize(routeName.split('.')[1]);
 
