e20a4520d342a1e46514324b7ae116280c9c54cb
Duncan Graham Walker <walkerdu@bc.edu>
2017-03-06 22:26:17 -0500

    Ember CLI upgraded to 2.11.1

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -5,12 +5,16 @@ module.exports = function(environment) {
     modulePrefix: 'dummy',
     podModulePrefix: 'dummy/pods',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
 
@@ -38,7 +42,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
@@ -49,7 +52,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/ember-google-charts';
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/ember-google-charts/';
+
   }
 
   return ENV;
