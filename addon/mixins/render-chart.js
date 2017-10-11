4c45dc7322a3ed59b97277be1061742cba77e143
Bruno Salgado <brunosalgado@cvc.com.br>
2017-10-11 14:57:34 -0300

    validate 'design' attribute value

--- addon/mixins/render-chart.js
+++ addon/mixins/render-chart.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import renderMaterialChart from '../utils/render-material-chart';
 import renderClassicChart from '../utils/render-classic-chart';
 
-const { set, getProperties, isBlank } = Ember;
+const { set, getProperties, isPresent, assert } = Ember;
 
 export default Ember.Mixin.create({
 
@@ -14,8 +14,10 @@ export default Ember.Mixin.create({
   _setRenderChart() {
     let {design, defaultDesign} = getProperties(this, ['design', 'defaultDesign'])
       , renderChartList = {'classic': renderClassicChart, 'material': renderMaterialChart}
-      , renderChartSelected = renderChartList[isBlank(design) ? defaultDesign : design];
+      , renderChartSelected = renderChartList[isPresent(design) ? design : defaultDesign]
+      , isValidRenderchart = isPresent(renderChartSelected);
 
+    assert(`Invalid 'design' value.`, isValidRenderchart);
     set(this, 'renderChart', renderChartSelected);
   },
 
