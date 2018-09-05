193e770c1c5a538267d0115ed55635fd7caa9236
Duncan Walker <duncanwalker@Duncans-MacBook-Pro.local>
2018-09-04 23:26:40 -0400

    Test suite passing for Ember CLI 3 update.

--- tests/integration/components/options-change-test.js
+++ tests/integration/components/options-change-test.js
@@ -1,7 +1,6 @@
-import { later } from '@ember/runloop';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render, find, settled, waitFor } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 const data = [
@@ -16,14 +15,12 @@ module('Integration | Component | chart options change', function(hooks) {
   setupRenderingTest(hooks);
 
   hooks.beforeEach(function() {
-    this.actions = {};
+    this.set('actions', {});
     this.send = (actionName, ...args) => this.actions[actionName].apply(this, args);
   });
 
   test('Changing options and rerender', async function(assert) {
     assert.expect(2);
-
-    const done = assert.async();
 
     const title = 'Some legit title';
 
@@ -32,27 +29,25 @@ module('Integration | Component | chart options change', function(hooks) {
       options: { title },
     });
 
-    this.actions.chartDidRender = () => {
+    await render(hbs`{{line-chart data=data options=options}}`);
 
-      later(() => {
-        assert.ok(find('div').innerHTML.indexOf(title) > -1, 'The component should have a title');
+    /* Check that the title is correct */
 
-        this.set('options', {});
+    await waitFor('.google-chart svg');
 
-        later(() => {
-          assert.ok(find('div').innerHTML.indexOf(title) === -1, 'The component should no longer have a title');
+    assert.ok(find('div').innerHTML.indexOf(title) > -1,
+      'The component should have a title');
 
-          done();
+    const newTitle = 'A new title';
 
-        }, 100);
+    this.set('options', {
+      title: newTitle,
+    });
 
-      }, 500);
+    await settled();
 
-      this.actions.chartDidRender = function() {};
-
-    };
-
-    await render(hbs`{{line-chart data=data options=options chartDidRender=(action 'chartDidRender')}}`);
+    assert.ok(find('div').innerHTML.indexOf(newTitle) > -1,
+      'The component should have a title');
 
   });
 });
